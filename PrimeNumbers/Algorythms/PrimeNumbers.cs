487265fbd9fce32bcd60cf96844ff1116dfd4904
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-03-24 20:59:35 +0300

    Вспомогательный коммит

--- PrimeNumbers/Algorythms/PrimeNumbers.cs
+++ PrimeNumbers/Algorythms/PrimeNumbers.cs
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Threading;
+
+namespace MetroFramework_test_at_a_new_project.Algorythms
+{
+    public static class PrimeNumbers
+    {
+        public static int[] GenerateInt(int n)
+        {
+            var res = new int[n];
+
+            Thread.Sleep(20000);
+            return res;
+        } 
+
+        public static string[] GenerateStr(int n) => new[]
+        {
+            1.ToString(),
+            2.ToString(),
+            3.ToString(),
+            5.ToString(),
+            7.ToString(),
+            11.ToString(),
+            13.ToString(),
+            17.ToString(),
+            19.ToString(),
+            23.ToString(),
+            29.ToString()
+        };
+    }
+}
+=======
 ﻿namespace MetroFramework_test_at_a_new_project.Algorythms
 {
     public static class PrimeNumbers
@@ -42,3 +75,4 @@
 
     }
 }
+>>>>>>> 6edb5c5ddbfec3c03b56018e462a655396ac6e72
