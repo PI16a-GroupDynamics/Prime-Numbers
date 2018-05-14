a1f07368250f20938d85ecbaa36f30bfeadcd0ef
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-05-15 01:34:45 +0300

    Deleted backups

--- PrimeNumbers/PrimeNumbers_Backup_2018.05.14_07.41.35/Printing/SettingsForResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MetroFramework_test_at_a_new_project.Printing
-{
-    static class SettingsForResult
-    {
-        public static string ItemResultSeparator { get; set; } = default;
-    }
-}
